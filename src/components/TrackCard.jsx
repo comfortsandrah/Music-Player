0ecba2b49544050f4e5819787662b01871a4bebe
Sandrah <comfortsandrah13@gmail.com>
2024-09-27 23:59:07 +0300

    Refactor TrackCard component layout and styling

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TrackCard = () => {
   return (
-    <div className=''>
-        <img src="./music.jpg" alt="image card" className='w-14 h-14'/>
-        <h3>Comfort's Music</h3>
+    <div className='flex flex-wrap justify-center items-center gap-3 p-3  max-w-52 max-h-64 ml-3 mt-3'>
+        <img src="./music.jpg" alt="image card" className='w-36 h-40 rounded-xl'/>
+        <h3 className='font-semibold'>Comfort's Music</h3>
     </div>
   )
 }
