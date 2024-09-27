da9e3ddbd887eef692cb73772b60420eae262c56
Sandrah <comfortsandrah13@gmail.com>
2024-09-28 01:20:12 +0300

    Refactor TrackCard component to use props for image, title, and description

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const TrackCard = () => {
+const TrackCard = ({image, title,description}) => {
   return (
-    <div className='flex flex-wrap justify-center items-center gap-3 p-3  max-w-52 max-h-64 ml-3 mt-3'>
-        <img src="./music.jpg" alt="image card" className='w-36 h-40 rounded-xl'/>
-        <h3 className='font-semibold'>Comfort's Music</h3>
+    <div className='flex flex-col gap-2 rounded-xl justify-center min-w-48 min-h-64 mt-1 text-left hover:shadow hover:cursor-pointer'>
+        <img src={image} alt={title} className='w-40 h-44 mx-auto rounded-xl'/>
+        <div className='flex flex-col p-3'>
+
+        <h3 className='font-semibold'>{title}</h3>
+        <p className='text-muted'>{description}</p>
+        </div>
     </div>
   )
 }
