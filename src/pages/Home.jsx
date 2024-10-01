62036145d8bc766f3fb39a63cfc1dad5b7410796
Sandrah <comfortsandrah13@gmail.com>
2024-10-02 00:44:18 +0300

    Refactor Home component to include SearchResults component and update formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import TrackCard from '../components/TrackCard'
-import TrackListing from '../components/TrackListing'
+import React from "react";
+import TrackCard from "../components/TrackCard";
+import TrackListing from "../components/TrackListing";
+import { SearchResults } from "../components/SearchBar";
 
 const Home = () => {
   return (
-    <section className='h-[90vh] overflow-scroll'>
-    <TrackListing/>
-     </section>
-  )
-}
+    <section className="h-[90vh] overflow-scroll">
+      <SearchResults />
+      <TrackListing />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
