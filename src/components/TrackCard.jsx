247a4d9de6dd1700b54f6b5d30f44d8e7e546b22
Sandrah <comfortsandrah13@gmail.com>
2024-10-04 22:29:06 +0300

    Refactor TrackCard component to include track preview playback functionality

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import { useMusicPlayer } from "../Context/MusicPlayerContext";
 
-const TrackCard = ({image, title,description}) => {
+const TrackCard = ({ preview, image, title, description }) => {
+  const { playTrack } = useMusicPlayer();
   return (
-    <div className='flex flex-col gap-2 rounded-xl justify-center max-w-[220px] max-h-[370px] mt-1 text-left hover:shadow hover:cursor-pointer mb-3 p-3'>
-        <img src={image} alt={title} className='w-40 h-44 mx-auto rounded-xl'/>
-        <div className='flex flex-col p-3'>
-
-        <h3 className='font-semibold'>{title}</h3>
-        <p className='text-muted'>{description}</p>
-        </div>
+    <div
+      className="flex flex-col gap-2 rounded-xl justify-center max-w-[220px] max-h-[370px] mt-1 text-left hover:shadow hover:cursor-pointer mb-3 p-3"
+      onClick={() => playTrack(preview,title,description)}
+      role="button"
+      aria-label={`Play track: ${title}`}
+    >
+      <img src={image} alt={title} className="w-40 h-44 mx-auto rounded-xl" />
+      <div className="flex flex-col p-3">
+        <h3 className="font-semibold">{title}</h3>
+        <p className="text-muted">{description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrackCard
+export default TrackCard;
