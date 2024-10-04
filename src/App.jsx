003df8428191c0ea320497a76e1c05f58eee1029
Sandrah <comfortsandrah13@gmail.com>
2024-10-04 22:31:44 +0300

    Refactor App component to include MusicPlayerProvider for controlling audio playback

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './Layout';
-import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import { MusicPlayerProvider } from "./Context/MusicPlayerContext";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Layout/>}>
-    <Route index element={<Home/>}/>
-    </Route>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
