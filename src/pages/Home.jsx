cdfee104ed2a44eacb9212ac4b5f02057060a1ad
Sandrah <comfortsandrah13@gmail.com>
2024-10-06 20:55:29 +0300

    Refactor Home component to remove commented out code and unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import TrackListing from "../components/TrackListing";
 import { SearchResults } from "../components/SearchBar";
 import MusicPlayer from "../components/MusicPlayer";
-// import { MusicPlayerProvider } from "../Context/MusicPlayerContext";
 
 const Home = () => {
   return (
     <section className="h-[90vh] overflow-scroll">
       <SearchResults />
       <h2 className="font-bold text-3xl my-3 p-6">Made for you</h2>
-      {/* <MusicPlayerProvider> */}
       <TrackListing />
       <MusicPlayer />
-      {/* </MusicPlayerProvider> */}
     </section>
   );
 };
