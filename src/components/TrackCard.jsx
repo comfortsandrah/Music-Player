7c16d2c36f7d476d4e38361de9d5d6e662aa3eee
Sandrah <comfortsandrah13@gmail.com>
2024-10-11 23:01:51 +0300

    Refactor TrackCard component to use responsive styles and update image dimensions

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -4,12 +4,12 @@ const TrackCard = ({ preview, image, title, artistName }) => {
   const { playTrack } = useMusicPlayer();
   return (
     <div
-      className="flex flex-col gap-2 rounded-xl justify-center max-w-[220px] max-h-[370px] mt-1 text-left hover:shadow hover:cursor-pointer mb-3 p-3"
+      className="flex flex-col gap-2 rounded-xl justify-center max-w-[180px] max-h-[330px] md:max-w-[220px] md:max-h-[370px] mt-1 text-left hover:shadow hover:cursor-pointer mb-3 p-3 bg-pink-900"
       onClick={() => playTrack(preview,title,artistName)}
       role="button"
       aria-label={`Play track: ${title}`}
     >
-      <img src={image} alt={title} className="w-40 h-44 mx-auto rounded-xl" />
+      <img src={image} alt={title} className="w-30 h-34 md:w-40 md:h-44 mx-auto rounded-xl" />
       <div className="flex flex-col p-3">
         <h3 className="font-semibold">{title}</h3>
         <p className="text-muted">{artistName}</p>
