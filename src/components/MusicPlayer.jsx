62f26bfe4f37470017d3fdc0b318e2c8ff48fd4b
Sandrah <comfortsandrah13@gmail.com>
2024-10-11 22:57:55 +0300

    Refactor MusicPlayer component to use artistName instead of artist in playTrack function

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -7,9 +7,17 @@ import { IoMdSkipForward } from "react-icons/io";
 import { SlLoop } from "react-icons/sl";
 import { useMusicPlayer } from "../Context/MusicPlayerContext";
 import VolumeSlider from "./Volume";
+import VoiceControl from "./VoiceControl";
 
 const MusicPlayer = () => {
-  const { currentTrack, currentTitle, currentArtist, tracks, playTrack } = useMusicPlayer();
+  const {
+    currentTrack,
+    currentTitle,
+    currentArtist,
+    tracks,
+    playTrack,
+    volume,
+  } = useMusicPlayer();
   const [isPlaying, setIsPlaying] = useState(false);
   const [isShuffle, setIsShuffle] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
@@ -18,7 +26,9 @@ const MusicPlayer = () => {
   const progressBarRef = useRef(null);
 
   // Get the current track index from the tracks list
-  const currentTrackIndex = tracks.findIndex((track) => track.preview === currentTrack);
+  const currentTrackIndex = tracks.findIndex(
+    (track) => track.preview === currentTrack
+  );
 
   const togglePlayPause = () => {
     if (isPlaying) {
@@ -38,13 +48,22 @@ const MusicPlayer = () => {
     } else {
       nextIndex = (currentTrackIndex + 1) % tracks.length;
     }
-    playTrack(tracks[nextIndex].preview, tracks[nextIndex].title, tracks[nextIndex].artist);
+    playTrack(
+      tracks[nextIndex].preview,
+      tracks[nextIndex].title,
+      tracks[nextIndex].artist.name
+    );
     setIsPlaying(true);
   };
 
   const skipBackward = () => {
-    let prevIndex = currentTrackIndex === 0 ? tracks.length - 1 : currentTrackIndex - 1;
-    playTrack(tracks[prevIndex].preview, tracks[prevIndex].title, tracks[prevIndex].artist);
+    let prevIndex =
+      currentTrackIndex === 0 ? tracks.length - 1 : currentTrackIndex - 1;
+    playTrack(
+      tracks[prevIndex].preview,
+      tracks[prevIndex].title,
+      tracks[prevIndex].artist.name
+    );
     setIsPlaying(true);
   };
 
@@ -84,43 +103,57 @@ const MusicPlayer = () => {
       setIsPlaying(false);
     }
   }, [currentTrack]);
+  // if (volume) audioRef.current.volume = 1;
 
   return (
-    <div className="shadow bottom-0 sticky z-10 bg-white p-4">
+    <div className="shadow bottom-0 sticky z-10 bg-white md:p-4 p-3 ml-3">
       {currentTitle && currentArtist && (
         <div>
           <h3 className="font-semibold text-xl">{currentTitle}</h3>
-          <p className="font-base">By {currentArtist.name}</p>
+          <p className="font-base">By {currentArtist}</p>
         </div>
       )}
-      <div className="flex justify-between items-center max-w-96 mx-auto text-2xl">
+      <div className="flex justify-between items-center max-w-80 md:max-w-96 mx-auto md:text-2xl">
         <TiArrowShuffle
           className={`cursor-pointer ${isShuffle ? "text-pink-500" : ""}`}
           onClick={toggleShuffle}
         />
-        <div className="flex gap-6 text-2xl items-center">
+        <div className="flex md:gap-6 md:text-2xl items-center">
           <IoMdSkipBackward onClick={skipBackward} className="cursor-pointer" />
           {isPlaying ? (
-            <IoPauseCircle onClick={togglePlayPause} className="cursor-pointer" />
+            <IoPauseCircle
+              onClick={togglePlayPause}
+              className="cursor-pointer"
+            />
           ) : (
-            <IoIosPlayCircle onClick={togglePlayPause} className="cursor-pointer" />
+            <IoIosPlayCircle
+              onClick={togglePlayPause}
+              className="cursor-pointer"
+            />
           )}
           <IoMdSkipForward onClick={skipForward} className="cursor-pointer" />
         </div>
         <SlLoop
           className={`cursor-pointer ${isLooping ? "text-pink-500" : ""}`}
           onClick={toggleLoop}
-          />
-        <VolumeSlider/>
+        />
+        <div>
+          <VoiceControl />
+          <VolumeSlider />
+        </div>
       </div>
-      <div className="w-full bg-gray-200 rounded-full h-2.5 mt-4 mb-2">
+      <div className="sm:w-28 md:w-full bg-gray-200 rounded-full h-2.5 mt-4 mb-2">
         <div
           ref={progressBarRef}
           className="bg-pink-500 h-2.5 rounded-full"
           style={{ width: `${progress}%` }}
-          ></div>
+        ></div>
       </div>
-      <audio ref={audioRef} onTimeUpdate={updateProgress} onEnded={handleTrackEnd} />
+      <audio
+        ref={audioRef}
+        onTimeUpdate={updateProgress}
+        onEnded={handleTrackEnd}
+      />
     </div>
   );
 };
