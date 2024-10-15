1f724e3ca8536aea1aae8e1964f01d5becaa2ecc
Sandrah <comfortsandrah13@gmail.com>
2024-10-15 20:27:18 +0300

    Refactor Layout component to add a responsive search bar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,14 +3,17 @@ import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import { Outlet } from "react-router-dom";
 import { MusicPlayerProvider } from "./Context/MusicPlayerContext";
+import SearchBar from "./components/SearchBar";
 
 const Layout = () => {
   return (
     <MusicPlayerProvider>
       <NavBar />
-      <div className="flex">
-      <SideBar />
-      <Outlet />
+      <div className="md:hidden m-2 mt-16 bg-white mx-auto w-full rounded-full fixed ">
+      <SearchBar/>
+      </div>
+      <div>
+        <Outlet />
       </div>
     </MusicPlayerProvider>
   );
