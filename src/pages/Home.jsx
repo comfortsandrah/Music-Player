6f1ad2dc9c6f3c137c766317bea38152f3d97bac
Sandrah <comfortsandrah13@gmail.com>
2024-10-15 20:32:20 +0300

    Refactor Home component to update layout and styling for responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import TrackListing from "../components/TrackListing";
-import { SearchResults } from "../components/SearchBar";
+import SearchBar, { SearchResults } from "../components/SearchBar";
 import MusicPlayer from "../components/MusicPlayer";
 
 const Home = () => {
   return (
-    <section className="h-[90vh] overflow-scroll">
-      <SearchResults />
-      <h2 className="font-bold text-3xl my-3 p-6">Made for you</h2>
+    <section className="h-[137vh] mx-autoflex-grow">     
+      <SearchResults />      
+      <h2 className="font-semibold md:font-bold text-xl md:text-3xl my-1 md:my-3 p-2 md:p-6 ml-3">Made for you</h2>      
       <TrackListing />
       <MusicPlayer />
     </section>
