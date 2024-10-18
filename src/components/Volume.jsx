abbe034315b4763eebfc15025e294a5f80d36a7d
Sandrah <comfortsandrah13@gmail.com>
2024-10-18 20:59:02 +0300

    Refactor Volume component to use useRef hook and integrate with MusicPlayerContext

--- src/components/Volume.jsx
+++ src/components/Volume.jsx
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { Howler } from "howler";
+import React, { useEffect, useRef } from "react";
 import { FaVolumeHigh, FaVolumeLow, FaVolumeXmark } from "react-icons/fa6";
+import { useMusicPlayer } from "../Context/MusicPlayerContext";
 
 const VolumeSlider = () => {
-  const [volume, setVolume] = useState(10);
-  const [isDragging, setIsDragging] = useState(false);
+  const { volume, setVolume } = useMusicPlayer(); // Initial volume set to 50 for a mid-point
+  const audioRef = useRef(null);
 
-  
   useEffect(() => {
-    Howler.volume(volume / 100);
+    if (audioRef.current) {
+      // audioRef.current.volume(volume/100);
+    }
   }, [volume]);
 
-
   const handleChange = (e) => {
-    setVolume(Number(e.target.value));
+    const newVolume = Number(e.target.value);
+    setVolume(newVolume);
   };
+
   return (
     <div className="flex gap-x-2 items-center">
       {volume === 0 && <FaVolumeXmark />}
-      {volume < 50 && volume > 0 && <FaVolumeLow />}
+      {volume > 0 && volume < 50 && <FaVolumeLow />}
       {volume >= 50 && <FaVolumeHigh />}
-      <div className="w-36 h-2 bg-slate-300 rounded-xl">
+      <div className="w-16 md:w-36 h-2 bg-slate-300 rounded-xl relative">
         <div
           className="bg-pink-500 h-2 rounded-full"
           style={{ width: `${volume}%` }}
-        >
-          <input
-            type="range"
-            min={0}
-            max={100}
-            value={volume}
-            onChange={handleChange}
-            className="cursor-pointer w-full absolute top-0 left-0 opacity-0.9"
-          />
-        </div>
+        ></div>
+        <input
+          type="range"
+          min={0}
+          max={100}
+          value={volume}
+          onChange={handleChange}
+          className="cursor-pointer md:w-full absolute top-0 left-0 opacity-0"
+        />
       </div>
     </div>
   );
