b4463066b51c9bcf766647119b80fcf3c0c595e4
Sandrah <comfortsandrah13@gmail.com>
2024-10-18 22:39:05 +0300

    Refactor DarkMode component to use icons for toggling dark mode

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
 function DarkMode() {
   const [darkMode, setDarkMode] = useState(false);
 
   // Check and load user preference on component mount
   useEffect(() => {
-    const userTheme = localStorage.getItem('theme');
-    if (userTheme === 'dark') {
+    const userTheme = localStorage.getItem("theme");
+    if (userTheme === "dark") {
       setDarkMode(true);
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add("dark");
     }
   }, []);
 
@@ -16,22 +18,26 @@ function DarkMode() {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     if (!darkMode) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   };
 
   return (
-    <div className="bg-white dark:bg-gray-900 transition-all duration-300">
-        <button
-          className=" bg-gray-300 dark:bg-gray-700 p-2 rounded-full text-sm"
-          onClick={toggleDarkMode}
-        >
-          Toggle Dark Mode
-        </button>
+    <div className="bg-white dark:bg-gray-700 transition-all duration-300">
+      <button
+        className=" bg-gray-300 dark:bg-gray-600 p-2 rounded-full text-sm"
+        onClick={toggleDarkMode}
+      >
+        {darkMode ? (
+          <MdLightMode className="text-lg md:text-xl" />
+        ) : (
+          <MdDarkMode className="tet-lg md:text-xl" />
+        )}
+      </button>
     </div>
   );
 }
